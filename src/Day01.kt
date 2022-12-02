8380822f021d733abe988d6f848555f4c1dbd9d6
Tomasz Przybysz <tomasz.przybysz@gmail.com>
2022-12-02 01:58:34 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
+import java.util.concurrent.atomic.AtomicInteger
+
 fun main() {
+
+    fun elfInventories(input: List<String>): List<Int> {
+
+        val elves = ArrayList<AtomicInteger>().apply { add(AtomicInteger()) }
+
+        input.forEach { line: String ->
+
+            if (line.isEmpty()) {
+                elves.add(AtomicInteger())
+            } else {
+                elves.last().addAndGet(line.toInt())
+            }
+        }
+
+        return elves.map(AtomicInteger::get)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return elfInventories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return elfInventories(input)
+            .sortedDescending()
+            .subList(0, 3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
